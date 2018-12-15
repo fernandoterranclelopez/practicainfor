d8b34d4b9deca0807ca0a7b0ba4847a426f7086a
fernandoterranclelopez <ferchuterrancle@gmail.com>
2018-12-15 13:25:14 +0100

    otro holamundo

--- holamundo.java
+++ holamundo.java
@@ -1,3 +1,4 @@
 public class HolaMundo{
-aaapublic static void main(String[] args){ System.out.println("Hola 
+aaapublic static void main(String[] args){ System.out.println("Otro Hola 
 mundo"); }
+}
