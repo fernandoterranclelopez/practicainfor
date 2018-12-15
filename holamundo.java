59384c21a686ed91b12f239b2ef93352cb2f3537
fernandoterranclelopez <ferchuterrancle@gmail.com>
2018-12-15 20:11:17 +0100

    Cambios solucionados en HolaMundo

--- holamundo.java
+++ holamundo.java
@@ -1,8 +1,6 @@
 public class HolaMundo{
-<<<<<<< HEAD
-aaapublic static void main(String[] args){ System.out.println("Otro Hola 
-=======
-aaapublic static void main(String[] args){ System.out.print("Nuevo Hola 
->>>>>>> origin/master
-mundo"); }
+
+public static void main(String[] args){
+ System.out.println("Otro Hola mundo.");
+} 
 }
